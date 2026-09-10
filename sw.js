bd36d4d04ca0dfbd7a4423e20f3b9c7347c07f66
ygraham181 <ygraham181@gmail.com>
2026-09-10 08:56:16 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "bus-run-app-v1";
+const CACHE_NAME = "orbit-buschief-v2";
 const APP_SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
